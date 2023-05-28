4448bfd34c6280270664922f6d7c898ec41071ed
klaudilag <klaudilag@wp.pl>
2023-05-28 17:56:58 +0200

    Initial commit

---
+++ src/main/java/com/example/kodilla_manyprojects/KodillaManyprojectsApplication.java
@@ -0,0 +1,13 @@
+package com.example.kodilla_manyprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaManyprojectsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaManyprojectsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kodilla_manyprojects/KodillaManyprojectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kodilla_manyprojects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaManyprojectsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
